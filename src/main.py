a0ed78db73fc41ea7b8e515bf22b4e9273463915
aash1kkk <aashikmohammed700@gmail.com>
2026-05-11 12:25:45 +0530

    day 2 and 3

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from src.redis_client import r
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Sales Proposal AI Running"}
+    return {"message": "Running"}
 
+@app.get("/cache")
+def cache_test():
+    r.set("test_key", "Hello Redis")
+    value = r.get("test_key")
+    return {"cached_value": value}
